74f94f81a68c0f2421c0e5bdb6ac540ac6339fe1
Brahian Julian Mejia Perez <brayantqm123@gmail.com>
2019-12-04 16:48:39 -0500

    Subida de la tarea donde se realiza un página adaptable al tamaño de la ventana

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>
+           SpaceX
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    </head>
+    <body>
+        <div id="pagewrap">
+                <img id="logo1" src="images/logo.png">
+                <img id="logo2" src="images/logo.png">
+    <center>
+        <div id="header">
+
+                <h1>SpaceX</h1>
+                <p>Inicio - Mision - Vision - Contactenos</p>
+                
+        </div>
+    </center>
+<div id="content">
+            <h2>Historia</h2>
+            <p>Space Exploration Technologies Corporation es una empresa estadounidense de transporte aeroespacial fundada en 2002 por Elon Musk, quien es también cofundador de PayPal, y fundador de Tesla Motors, SolarCity, Hyperloop, The Boring Company y OpenAI.</p>
+        
+            <h2>Cosas Logradas</h2>
+            <p>Ha desarrollado los cohetes Falcon 1, Falcon 9 y Falcon Heavy los cuales han sido construidos con la meta de ser vehículos de lanzamiento espacial reutilizables. SpaceX también ha desarrollado la nave espacial Dragon que fue puesta en órbita por los vehículos de lanzamiento Falcon 9, también se encuentran desarrollando el BFR o Star Ship una nave la cual planea usar para enviar colonias de humanos en futuras misiones a Marte. SpaceX diseña, prueba y fabrica la mayor parte de los componentes por cuenta propia, incluyendo los motores de cohete Merlín, Kestrel y Draco.</p>
+
+        <div id="valor">
+            <h2>Empresa Similar</h2>
+            Blue Origin es una empresa estadounidense de transporte aeroespacial fundada en el 2000 por Jeff Bezos, también fundador de Amazon. Entre sus objetivos se encuentran los vuelos sub orbitales y orbitales, tanto para misiones oficiales de Estados Unidos, como para vuelos privados, Su principal trabajo son los cohetes reutilizables por medio de descensos controlados de los mismos por propulsión luego de los lanzamientos.
+        </div>            
+        
+        <div id="lista">
+            <h2>Algunos Logros</h2>
+            <p>El primer cohete de financiación privada en alcanzar la órbita.
+                <p>    
+                La primera empresa privada en enviar una nave espacial a la Estación Espacial Internacional.
+                <p>    
+                La primera empresa privada en enviar un satélite a una órbita geosíncrona.
+                <p>    
+                El primer aterrizaje de la primera fase de un cohete orbital sobre suelo firme.
+                <p>    
+                El primer aterrizaje de la primera fase de un cohete orbital sobre una plataforma en el océano.
+                <p>   
+                El primer lanzamiento y aterrizaje de un cohete usado.
+                <p>
+            </p>
+
+        </div>
+</div>
+
+        <div id="sidebar1">
+
+            <h3>Algo de historia</h3>
+            <p>En 2001, Elon Musk conceptualizó Mars Oasis, un proyecto para enviar un invernadero experimental en miniatura a Marte y cultivar plantas en un intento para recuperar el interés público en la exploración espacial y aumentar el presupuesto de la NASA. Musk intentó comprar cohetes baratos de Rusia pero volvió con las manos vacías al ser incapaz de encontrarlos a un precio aceptable.
+        </div>
+
+        <div id="sidebar2">
+
+            <h3>Contratiempos</h3>
+            <p>
+            En marzo de 2013 una cápsula Dragon tuvo problemas con los propulsores debido a unas válvulas bloqueadas. Los ingenieros de SpaceX fueron capaces de arreglarlo remotamente pero debido a este problema la nave llegó un día más tarde de lo esperado a la Estación Espacial Internacional.
+            <p>
+                En septiembre de 2016, un Falcon 9 explotó en la plataforma de lanzamiento durante el proceso de repostaje para la última prueba antes del lanzamiento. La carga, el Amos-6 de Spacecom valorado en 200 millones de dólares, fue destruida. Elon Musk describió el evento como "el fallo más complejo y difícil" en la historia de SpaceX.
+        </p>
+        
+        </div>
+<center>
+        <div id="footer">
+                <img id="botella1" src="images/spacex.jpg">
+                <img id="botella2" src="images/spacex.jpg">
+        <p>
+            Contacto:
+            <p>
+                SpaceX 310-363-6000
+            <p>
+                media@spacex.com / sales@spacex.com
+            <p>
+                Brahian Julian Mejia Perez
+            <p>
+                Analisis y Desarrollo de Software
+            <p>
+                CENSA
+        </p>
+        
+        </div>
+</center>
+
+</div>
+
+        
+    </body>
+</html>
